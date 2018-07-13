26187589025afce3d0403ea386860687775bf55b
Thanh Pham <tienthanh.dqc@gmail.com>
2018-07-13 15:59:44 +0700

    Add salt

--- scripts/util/generate_config.py
+++ scripts/util/generate_config.py
@@ -1,14 +1,20 @@
+import uuid
+
 from jinja2 import Template
 from namesgenerator import get_random_name
+
+def get_random_str(block=1):
+    s = uuid.uuid4()
+    return str(s).split('-')[block]
 
 data = {
     'nbr_compute_node': 2,
     'nbr_block_node': 1,
     'base_fqdn': 'openstack.dc4.tma.com.vn',
-    'passwd_default': "%s" % get_random_name(),
-    'passwd_database': "%s" % get_random_name(),
-    'passwd_service': "%s" % get_random_name(),
-    'passwd_admin': "%s" % get_random_name(),
+    'passwd_default': "%s_%s" % (get_random_name(), get_random_str(4)),
+    'passwd_database': "%s_%s" % (get_random_name(), get_random_str(0)),
+    'passwd_service': "%s_%s" % (get_random_name(), get_random_str(4)),
+    'passwd_admin': "%s_%s_%s" % (get_random_str(1), get_random_name(), get_random_str(1)),
     'passwd_demo': "%s" % get_random_name(),
 
 }
